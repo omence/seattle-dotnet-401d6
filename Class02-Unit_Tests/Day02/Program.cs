0627d8d71d4c9b6ef5e18924d5c054dea9738a3e
vinicio@codefellows.com <vinicio@codefellows.com>
2019-01-08 17:00:02 -0800

    Day 02

---
+++ Class02-Unit_Tests/Day02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Day02
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string FizzBuzz(int num)
+        {
+            if(num % 3 == 0 && num % 5 == 0)
+            {
+                return "FizzBuzz";
+            } 
+            else if(num % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if(num % 5 == 0)
+            {
+                return "Buzz";
+            } 
+            return num.ToString();
+        }
+    }
+}
