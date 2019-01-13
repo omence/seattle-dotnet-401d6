46da612b035e01134647c0d3714beb0674029701
Jason Few <omence11@gmail.com>
2019-01-13 09:10:31 -0800

    Added Xunit

---
+++ Class04-Classes_Objects/StarterCode/Lab04_TicTacToe/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using Lab04_TicTacToe;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+
+
+        }
+    }
+}
